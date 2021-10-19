8d16c19c4db067506fbd0e2bfde48c39addc3ef4
Russell Buchanan <russell@robots.ox.ac.uk>
2021-10-19 14:41:34 +0100

    Some clean up and adds map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,18 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "McGill University, Montreal, Canada",
+    345.503826, -73.574811
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "ETH Zürich, Zürich, Switzerland",
+    47.376548, 8.548597
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "CSIRO Data 61, Brisbane, Australia",
+    -27.529286, 152.910654
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Saarland University, Saarbrücken, Germany",
+    49.254770, 7.041470
   ]
 ];
